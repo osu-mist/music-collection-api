c2ccabf8070267fdf47688e632f6ccb276526fcf
Andrew Ekstedt <ekstedta@oregonstate.edu>
2016-05-05 17:11:04 -0700

    Rename package and application class.
    webapiskeleton => musicapi
    SkeletonApplication => MusicApplication

--- src/main/groovy/edu/oregonstate/mist/musicapi/MusicApplication.groovy
+++ src/main/groovy/edu/oregonstate/mist/musicapi/MusicApplication.groovy
@@ -1,4 +1,4 @@
-package edu.oregonstate.mist.webapiskeleton
+package edu.oregonstate.mist.musicapi
 
 import edu.oregonstate.mist.api.Configuration
 import edu.oregonstate.mist.api.Resource
@@ -6,7 +6,7 @@ import edu.oregonstate.mist.api.InfoResource
 import edu.oregonstate.mist.api.AuthenticatedUser
 import edu.oregonstate.mist.api.BasicAuthenticator
 import edu.oregonstate.mist.api.NullHealthCheck
-import edu.oregonstate.mist.webapiskeleton.resources.SampleResource
+import edu.oregonstate.mist.musicapi.resources.SampleResource
 import io.dropwizard.Application
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
@@ -16,7 +16,7 @@ import io.dropwizard.auth.basic.BasicAuthFactory
 /**
  * Main application class.
  */
-class SkeletonApplication extends Application<Configuration> {
+class MusicApplication extends Application<Configuration> {
     /**
      * Initializes application bootstrap.
      *
@@ -40,7 +40,7 @@ class SkeletonApplication extends Application<Configuration> {
                 AuthFactory.binder(
                         new BasicAuthFactory<AuthenticatedUser>(
                                 new BasicAuthenticator(configuration.getCredentialsList()),
-                                'SkeletonApplication',
+                                'MusicApplication',
                                 AuthenticatedUser.class)))
         // Shut the health check warning up
         environment.healthChecks().register("null", new NullHealthCheck())
@@ -53,6 +53,6 @@ class SkeletonApplication extends Application<Configuration> {
      * @throws Exception
      */
     public static void main(String[] arguments) throws Exception {
-        new SkeletonApplication().run(arguments)
+        new MusicApplication().run(arguments)
     }
 }

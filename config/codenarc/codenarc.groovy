621287de0ba32db9143986a5ce932a01f3d8ecf0
Andrew Ekstedt <ekstedta@oregonstate.edu>
2016-05-18 12:06:17 -0700

    Remove codenarc rules specific to the skeleton project.
    We've renamed the package and the Sample classes, so no need to check
    for them any longer.

--- config/codenarc/codenarc.groovy
+++ config/codenarc/codenarc.groovy
@@ -4,28 +4,19 @@
 ruleset {
     description 'MIST Groovy RuleSet'
 
-    ClassName {
-        description = 'Verify that class name starts with an uppercase letter followed by zero or ' +
-                      'more word characters or dollar signs, warning if skeleton code is unchanged.'
-        regex = '''(?x)
-                   ^(
-                     (?!(SkeletonApplication|Sample|SampleResource)$) # fail on skeleton class names
-                     ([A-Z]\\w*$?)*                                   # codenarc default
-                   )$'''
-        priority = 3
-    }
+    /* Verify that class name starts with an uppercase letter followed by zero or
+       more word characters or dollar signs. */
+    ClassName
 
     /* method name starts with a lowercase letter */
     MethodName
 
     PackageName {
         description = 'Verify that package name begins with \'edu.oregonstate.mist.\' and consists ' +
-                      'only of lowercase letters and numbers separated by periods, warning if ' +
-                      'skeleton code is unchanged.'
+                      'only of lowercase letters and numbers separated by periods.'
         regex = '''(?x)
                    ^(
                      edu\\.oregonstate\\.mist # begin with edu.oregonstate.mist
-                     (?!\\.webapiskeleton)    # fail on skeleton package name
                      (\\.[a-z0-9]+)*          # periods separate lowercase alphanumeric package names
                    )$'''
         packageNameRequired = true
